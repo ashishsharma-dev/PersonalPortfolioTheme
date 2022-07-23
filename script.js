dfc3fb0c8905cae5d1b2657311244a4a2de8a22d
Ashish Sharma <55922529+ashishsharma-dev@users.noreply.github.com>
2022-07-23 09:43:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const customPreloader = document.querySelector('.customPreloader');
+document.body.style.overflow = 'hidden';
+
+
+window.onscroll = function(e) { 
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    var customNav = document.querySelector('.customNav');
+
+    scrollY <= this.lastScroll 
+      ? customNav.style.visibility = 'visible'
+      : customNav.style.visibility = 'hidden'; 
+
+    this.lastScroll = scrollY ;
+}
+
+
+setTimeout(()=>{
+  document.body.style.overflowY = 'scroll';
+  customPreloader.style.display = 'none';
+},4000);
